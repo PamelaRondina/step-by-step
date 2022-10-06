a331becc3f0967449c506a423987661cc8e52414
Pamela Rondina <pamela.rondina@gmail.com>
2022-10-06 17:58:01 -0300

    delet

--- c#/README.md
+++ c#/README.md
@@ -5,3 +5,5 @@
 [Microsoft Documentação](https://learn.microsoft.com/pt-br/docs/)
 
 
+
+
---
+++ c#/hola_mundo.cs
@@ -0,0 +1,6 @@
+using System;
+public class HelloWorld {
+    public static void Main() {
+        Console.WriteLine("Hello World!");
+    }
+}
